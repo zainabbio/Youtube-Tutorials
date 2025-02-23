cc911e189f16ad45cbedb57fff6933a55768638a
Zainab Jan <115025383+zainabbio@users.noreply.github.com>
2025-02-23 23:13:10 +0300

    Created using Colab

---
+++ R_for_Genomic_Data_Analysis_R.R
@@ -0,0 +1,39 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyOUjRomIsf8gO/z7R0NOEKa",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/zainabbio/Youtube-Tutorials/blob/main/R_for_Genomic_Data_Analysis_R.R\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "yAeBarTUcF7v"
+      },
+      "outputs": [],
+      "source": []
+    }
+  ]
+}
